35ff4760f61b9f97179792231cfd3c8836500b63
Henry Cheung <cheetah_henry@hotmail.com>
2020-02-19 12:46:48 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,42 @@
+This repository consists of code for generating a lookup table (LUT) to be used for correcting ADC non-linearity issue. An example is also given on how to apply the LUT in your program.
+
+The code is based on original work from [Helmut Weber](https://github.com/MacLeod-D/ESP32-ADC) that he first described at [ESP32 discussion forum](https://esp32.com/viewtopic.php?f=19&t=2881&start=30#p47663), but modified with bug-fixed by me.
+
+### ESP32 Linearity issue
+
+To address the ESP32 ADC non-linear issue, a lookup table is used to correct the non-linearity. You may need to generate your own lookup table as it varies from device to device due to the variation of ESP32 internal reference voltage.
+
+[![ESP32 ADC linearity](https://github.com/e-tinkers/ntc-thermistor-with-arduino-and-esp32/blob/master/images/esp32_ADClinearity.png)](https://github.com/e-tinkers/ntc-thermistor-with-arduino-and-esp32/blob/master/images/esp32_ADClinearity.png)
+
+### Things need to know before using LUT
+
+It is recommended to read the [ESP32 Analog to Digital Converter](https://docs.espressif.com/projects/esp-idf/en/latest/api-reference/peripherals/adc.html) before using the LUT approach for solving the ESP32 linearity issue. According to documentation, ESP32 chips(ESP32-D0WD and ESP32-D0WDQ6) that manufactured after 1st week of 2018 have been individual measurement and burned with the eFuse Vref value on to the chip. The eFuse Vref can be read using the function call `read_efuse_vref(void)` which you can find the source code at [esp_adc_cal.c](https://github.com/espressif/esp-idf/blob/f91080637c054fa2b4107192719075d237ecc3ec/components/esp_adc_cal/esp_adc_cal.c#L153).
+
+There is a function [`calculate_voltage_linear()`](https://github.com/espressif/esp-idf/blob/f91080637c054fa2b4107192719075d237ecc3ec/components/esp_adc_cal/esp_adc_cal.c#L246) in the same library which uses a polynomial formula to correct the linearity.
+
+It is your choice on whether you want to implement the polynomial fitting method or LUT method to address your ESP32 ADC linearity problem. The LUT take up a lot of memory (which is not a big issue for ESP32) but it is faster and more accurate than the polynomial fitting approach.
+
+### How to generate the LUT?
+
+This program use the ESP32 DAC to generate a value as the reference and feed it into the ADC for calibration. Remember to use a short jumper wire to connect DAC output(pin 25) to ADC Channel 1(pin 35) on your ESP32.
+
+By default, the program will generate a `float ADC_LUT` which take up much more memory but with better precision, if you want to have an `int ADC_LUT` table, comment out the line `#define FLOAT_LUT` to get the integer table.
+
+1. Connect a jumper wire between pin25 and pin 35;
+2. decide on what table (float or integer) you'd want to generate;
+3. Start the `main.cpp` sketch from PlatformIO or Arduino IDE;
+4. When the program stops, copy and paste the entire table from Serial Monitor to your sketch to use it.
+
+### How to use the LUT?
+
+Refer to the sketch in example directory on how to apply the LUT in your program.
+
+### Noise on ESP32
+
+The ESP32 is much noisy compare to Arduino. Add a 0.1uF capacity on Vout would help a little bit in smoothing out the noise or alternative write a filter algorithm to handle it.
+
+**Arduino ADC reading from thermistor**
+[![Arduino ADC reading from thermistor](https://github.com/e-tinkers/ntc-thermistor-with-arduino-and-esp32/blob/master/images/Arduino_ADC_reading.png)](https://github.com/e-tinkers/ntc-thermistor-with-arduino-and-esp32/blob/master/images/Arduino_ADC_reading.png)
+
+**ESP32 ADC reading from thermistor**
+[![ESP32 ADC reading from thermistor](https://github.com/e-tinkers/ntc-thermistor-with-arduino-and-esp32/blob/master/images/ESP32_ADC_reading.png)](https://github.com/e-tinkers/ntc-thermistor-with-arduino-and-esp32/blob/master/images/ESP32_ADC_reading.png)
---
+++ src/main.cpp
@@ -0,0 +1,146 @@
+#include <Arduino.h>
+#include <driver/dac.h>
+
+// Based on original work from Helmut Weber (https://github.com/MacLeod-D/ESP32-ADC)
+// that he described at https://esp32.com/viewtopic.php?f=19&t=2881&start=30#p47663
+// Modified with bug-fixed by Henry Cheung
+//
+// Build a ESP32 ADC Lookup table to correct ESP32 ADC linearity issue
+// Run this sketch to build your own LUT for each of your ESP32, copy and paste the
+// generated LUT to your sketch for using it, see example sketch on how to use it
+
+// #define GRAPH        // uncomment this for print on Serial Plotter
+#define FLOAT_LUT    // uncomment this if you need float LUT
+
+float Results[4097];
+float Res2[4096*5];
+
+void dumpResults() {
+  for (int i=0; i<4096; i++) {
+    if (i % 16 == 0) {
+      Serial.println();
+      Serial.print(i); Serial.print(" - ");
+    }
+    Serial.print(Results[i], 2); Serial.print(", ");
+  }
+  Serial.println();
+}
+
+void dumpRes2() {
+  Serial.println(F("Dump Res2 data..."));
+  for (int i=0; i<(5*4096); i++) {
+      if (i % 16 == 0) {
+        Serial.println(); Serial.print(i); Serial.print(" - ");
+      }
+      Serial.print(Res2[i],3); Serial.print(", ");
+    }
+    Serial.println();
+}
+
+void setup() {
+    dac_output_enable(DAC_CHANNEL_1);    // pin 25
+    dac_output_voltage(DAC_CHANNEL_1, 0);
+    adcAttachPin(35);
+    Serial.begin(500000);
+    delay(1000);
+}
+
+void loop() {
+
+    Serial.print(F("Test Linearity "));
+    for (int j=0; j<500; j++) {
+      if (j % 100 == 0) Serial.print(".");
+      for (int i=0;i<256;i++) {
+          dac_output_voltage(DAC_CHANNEL_1, (i & 0xff));
+          delayMicroseconds(100);
+          adcStart(35);
+          while (adcBusy(35)) {
+            delayMicroseconds(10);
+          }
+          Results[i*16]=0.9*Results[i*16] + 0.1*adcEnd(35);
+      }
+    }
+    Serial.println();
+    // dumpResults();
+
+    Serial.println(F("Calculate interpolated values .."));
+    Results[4096] = 4095.0;
+    for (int i=0; i<256; i++) {
+       for (int j=1; j<16; j++) {
+          Results[i*16+j] = Results[i*16] + (Results[(i+1)*16] - Results[(i)*16])*(float)j / (float)16.0;
+       }
+    }
+    // dumpResults();
+
+    Serial.println(F("Generating LUT .."));
+    for (int i=0; i<4096; i++) {
+        Results[i]=0.5 + Results[i];
+    }
+    // dumpResults();
+
+    Results[4096]=4095.5000;
+    for (int i=0; i<4096; i++) {
+       for (int j=0; j<5; j++) {
+          Res2[i*5+j] = Results[i] + (Results[(i+1)] - Results[i]) * (float)j / (float)10.0;
+       }
+    }
+    // dumpRes2();
+
+    for (int i=1; i<4096; i++) {
+        int index;
+        float minDiff=99999.0;
+        for (int j=0; j<(5*4096); j++) {
+            float diff=fabs((float)(i) - Res2[j]);
+            if(diff<minDiff) {
+                minDiff=diff;
+                index=j;
+            }
+        }
+        Results[i]=(float)index;
+    }
+    // dumpResults();
+
+    for (int i=0; i<(4096); i++) {
+        Results[i]/=5;
+    }
+
+#ifdef GRAPH
+
+    while(1) {
+      for (int i=2; i<256; i++) {
+        dac_output_voltage(DAC_CHANNEL_1, (i & 0xff));
+        delayMicroseconds(100);
+        adcStart(35);
+        while (adcBusy(35)) {
+          delayMicroseconds(10);
+        }
+        float r = Results[adcEnd(35)];
+        Serial.print(i*16); Serial.print(" "); Serial.println(r);
+      }
+    }
+
+#else
+
+    Serial.println();
+
+#ifdef FLOAT_LUT
+    Serial.println("const float ADC_LUT[4096] PROGMEM = { 0,");
+    for (int i=1; i<4095; i++) {
+       Serial.print(Results[i],4); Serial.print(",");
+       if ((i%15)==0) Serial.println();
+    }
+    Serial.println(Results[4095]);
+    Serial.println("};");
+#else
+    Serial.println("const int ADC_LUT[4096] PROGMEM = { 0,");
+    for (int i=1; i<4095; i++) {
+      Serial.print((int)Results[i]); Serial.print(",");
+      if ((i%15)==0) Serial.println();
+    }
+    Serial.println((int)Results[4095]);
+    Serial.println("};");
+#endif
+    while(1);
+#endif
+
+}
